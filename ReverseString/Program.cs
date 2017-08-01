2da97278c2c383c53e4228e0724179895f5e152f
Dean <disorientedart@gmail.com>
2017-07-31 21:17:55 -0500

    Initial and final commit project completed

---
+++ ReverseString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string reverse = string.Empty;
+
+            Console.WriteLine(args[0]);
+            foreach(char letter in args[0])
+            {
+                reverse = string.Concat(letter, reverse);
+            }
+
+            Console.WriteLine(reverse);
+            Console.Read();
+        }
+    }
+}
